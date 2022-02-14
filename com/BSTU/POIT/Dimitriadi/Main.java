76c445ad1c885a8db7977782c77c98dd1c115fe9
Cyber-Terror <dimentr.anton@gmail.com>
2022-02-15 01:09:51 +0300

    Initial commit

---
+++ com/BSTU/POIT/Dimitriadi/Main.java
@@ -0,0 +1,10 @@
+package com.BSTU.POIT.Dimitriadi;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //TODO hello bro how are you
+        System.out.println("Hello world");
+    }
+}
